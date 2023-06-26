4b8325f3ed4b33b5dacb459fdf6d844bc6985114
Andry Krutsei~ <krucay06y@gmail.com>
2023-06-26 14:41:04 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const result = 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
+
+const email = "krucay06y@gmail.com";
+console.log(email.length);
+console.log(email.includes("@"));
+
+const firstPartOfWord = "My";
+const secondPartOfWord = "name";
+const thirdPartOfWord = "is";
+const fullName = `${firstPartOfWord} ${secondPartOfWord} ${thirdPartOfWord} Viktor`
+console.log(fullName);
+
+const userName = "Andriy";
+const payment = 800;
+alert(`Дякуємо, ${userName} ! До сплати ${payment}`)
+
